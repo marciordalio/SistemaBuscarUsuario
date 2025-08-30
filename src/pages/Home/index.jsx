8c863472025b27d31d96043a58f12ad79bb41cd3
Marcio Eduardo <marcioeduardo.o.c@gmail.com>
2025-08-29 21:40:19 -0300

    home index base pronta, map e function home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,73 @@
 import './style.css'
+import Trash from '../../assets/lixeira.png'
+
+function Home() {
+
+    const users = [{
+        id: '23232323',
+        name: 'JDK',
+        idade: 90,
+        email: 'code@gmail.com'
+    },
+
+    {
+        id: '232432323',
+        name: 'JDKdf',
+        idade: 903,
+        email: 'cod3rdde@gmail.com'
+    },
 
 
-function Home(){
-
-    return(
 
 
-        <div>
-    <h1> Hello World</h1>
-       </div>
+
+    ]
+
+    return (
+
+
+        <div className='GERAL'>
+
+
+            <div className='container'>
+                <form>
+                    <h1> Cadastro de Usuário</h1>
+                    <input name='nome' type='text'  placeholder="Nome" />
+                    <input idade='idade' type='number'  placeholder="Idade" />
+                    <input email='nome' type='email'  placeholder="email" />
+                    <button type='buttun'>  Cadastrar</button>
+                </form>
+            </div>
+
+
+        {users.map(user =>( 
+         <div key={user.id} className='cardsUsers' >
+             <p>Nome: {user.name}</p>
+             <p>Idade:{user.age} </p>
+             <p>Email: {user.email}</p>
+            <div className='delete'>
+                <button>
+                   <img src={Trash} alt='deletar'/> 
+                </button>
+            </div>
+         </div>
+      ))}
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
 
     )
 }
